9da13f3feb5bca5199ffd60079f778698714b421
Andrew Garcia <drew@drewgarcia.dev>
2023-05-13 13:50:18 -0400

    day 1

---
+++ day-1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,57 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// var = variable
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+// let is constant
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let score = 10
+let reallyBig = 100_000_000
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+print(counter)
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let newNumber = 0.1 + 0.2
+print(newNumber)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 5.0
+rating *= 2
