b5ce38b6b329adc487e0efc04fcd2affa99cc7c2
Andrew Garcia <drew@drewgarcia.dev>
2023-06-02 16:44:32 -0400

    day 15, day 16

---
+++ WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by drewdev on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
