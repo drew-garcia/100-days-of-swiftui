e0602fe5614d3869f15d085dc71dc816353f44d1
Andrew Garcia <drew@drewgarcia.dev>
2023-06-03 15:13:46 -0400

    day 18

---
+++ day-18/Day18/Day18/Day18App.swift
@@ -0,0 +1,17 @@
+//
+//  Day18App.swift
+//  Day18
+//
+//  Created by drewdev on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Day18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
