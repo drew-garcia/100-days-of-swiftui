bcfcd29cc42376723c18e0f8a532588feae26028
Andrew Garcia <drew@drewgarcia.dev>
2023-06-05 08:57:40 -0400

    day 20

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by drewdev on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
