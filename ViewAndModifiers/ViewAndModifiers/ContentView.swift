879b8ab0a534a5cd1f365d07347f231c8c18d6e3
Andrew Garcia <drew@drewgarcia.dev>
2023-06-08 16:13:34 -0400

    day 23

---
+++ ViewAndModifiers/ViewAndModifiers/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  ViewAndModifiers
+//
+//  Created by drewdev on 6/7/23.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .watermarked(with: "Andrew")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewAndModifiers/ViewAndModifiers/ViewAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiersApp.swift
+//  ViewAndModifiers
+//
+//  Created by drewdev on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
