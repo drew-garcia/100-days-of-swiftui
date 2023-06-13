9cf7d51dddeb942898f30d7ba1233d16128086e4
Andrew Garcia <drew@drewgarcia.dev>
2023-06-13 07:52:03 -0400

    day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by drewdev on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
