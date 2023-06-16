cab1bdc89bb2a507bf687f3ffc70e338175e6512
Andrew Garcia <drew@drewgarcia.dev>
2023-06-16 15:29:31 -0400

    day 38

---
+++ iExpense/iExpense/View-ExpenseStyling.swift
@@ -0,0 +1,20 @@
+//
+//  View-ExpenseStyling.swift
+//  iExpense
+//
+//  Created by drewdev on 6/15/23.
+//
+
+import SwiftUI
+
+extension View {
+    func style(for item: ExpenseItem) -> some View {
+        if item.amount < 10 {
+            return self.font(.body)
+        } else if item.amount < 100 {
+            return self.font(.title3)
+        } else {
+            return self.font(.title)
+        }
+    }
+}
