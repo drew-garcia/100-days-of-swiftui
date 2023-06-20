be774bcac0b99558277f42245922e3e46d4df156
Andrew Garcia <drew@drewgarcia.dev>
2023-06-20 12:11:45 -0400

    day 47

---
+++ Tabulae/Tabulae/ActivityView.swift
@@ -0,0 +1,43 @@
+//
+//  ActivityView.swift
+//  Tabulae
+//
+//  Created by drewdev on 6/20/23.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    @ObservedObject var data: Activities
+    var activity: Activity
+    
+    var body: some View {
+        List {
+            Section {
+                if activity.description.isEmpty == false {
+                    Text(activity.description)
+                }
+            }
+            
+            Section {
+                Text("Completion count: \(activity.completionCount)")
+                
+                Button("Mark Completed") {
+                    var newActivity = activity
+                    newActivity.completionCount += 1
+                    
+                    if let index = data.activities.firstIndex(of: activity) {
+                        data.activities[index] = newActivity
+                    }
+                }
+            }
+        }
+        .navigationTitle(activity.title)
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityView(data: Activities(), activity: Activity.example)
+    }
+}
---
+++ Tabulae/Tabulae/TabulaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabulaeApp.swift
+//  Tabulae
+//
+//  Created by drewdev on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TabulaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
