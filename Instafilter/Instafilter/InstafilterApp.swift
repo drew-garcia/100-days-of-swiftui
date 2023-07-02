1b94bfa19560e14025132c13e39c6240b5b653ad
Andrew Garcia <drew@drewgarcia.dev>
2023-07-02 16:20:17 -0400

    day 62

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by drewdev on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
