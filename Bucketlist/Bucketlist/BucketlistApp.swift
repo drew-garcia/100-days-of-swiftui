d74340812a81d34fbc32715cbc0b87dd2a42fa7d
Andrew Garcia <drew@drewgarcia.dev>
2023-07-03 14:01:45 -0400

    day 68

---
+++ Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by drewdev on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucketlist/Bucketlist/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Bucketlist
+//
+//  Created by drewdev on 7/3/23.
+//
+
+import SwiftUI
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct ContentView: View {
+    var loadingState = LoadingState.success
+    
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
