1f0df533fb8031cddc87e44df074d7f3ff9eb2b1
Andrew Garcia <drew@drewgarcia.dev>
2023-07-03 13:03:37 -0400

    day 66

--- Instafilter/Instafilter/ImageSaver.swift
+++ Instafilter/Instafilter/ImageSaver.swift
@@ -5,14 +5,21 @@
 //  Created by drewdev on 7/2/23.
 //
 
-import SwiftUI
+import UIKit
 
 class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
     func writeToPhotoAlbum(image: UIImage) {
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
     }
-
+    
     @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished!")
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
     }
 }
