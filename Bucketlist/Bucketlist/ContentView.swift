3084a540db31b999893c9abd5a328a763638f7d1
Andrew Garcia <drew@drewgarcia.dev>
2023-07-04 14:45:40 -0400

    day 69

--- Bucketlist/Bucketlist/ContentView.swift
+++ Bucketlist/Bucketlist/ContentView.swift
@@ -5,42 +5,42 @@
 //  Created by drewdev on 7/3/23.
 //
 
+import LocalAuthentication
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
-
 struct ContentView: View {
-    var loadingState = LoadingState.success
+    @State private var isUnlocked = false
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
         }
     }
 }
