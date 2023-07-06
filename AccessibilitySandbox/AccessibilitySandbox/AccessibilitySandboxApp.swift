bf22e4145241338beaf8ef20569cb633322073fb
Andrew Garcia <drew@drewgarcia.dev>
2023-07-06 08:08:25 -0400

    day 74

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by drewdev on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by drewdev on 7/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var value = 10
+    
+    var body: some View {
+        VStack {
+            Text("Value: \(value)")
+
+            Button("Increment") {
+                value += 1
+            }
+
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled.")
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
