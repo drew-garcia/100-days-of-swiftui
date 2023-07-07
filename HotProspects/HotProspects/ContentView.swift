e6a8a6028f76592c4c27ca8618898663cc0c2938
Andrew Garcia <drew@drewgarcia.dev>
2023-07-07 12:01:54 -0400

    day 77, 78, 79

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by drewdev on 7/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = "One"
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+                .tag("One")
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
