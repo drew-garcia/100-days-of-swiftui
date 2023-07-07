f37337c32583d9626d4bcc7653f8a512052a5dd3
Andrew Garcia <drew@drewgarcia.dev>
2023-07-07 18:47:55 -0400

    day 82

---
+++ HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,21 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by drewdev on 7/7/23.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
