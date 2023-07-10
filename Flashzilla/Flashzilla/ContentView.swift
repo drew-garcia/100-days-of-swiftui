8745d8a1b45966e720f30836a73a8c7c80d146cb
Andrew Garcia <drew@drewgarcia.dev>
2023-07-09 21:30:13 -0400

    day 87

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,16 +7,23 @@
 
 import SwiftUI
 
+func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ContentView: View {
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
-        }
-        .onTapGesture {
-            print("VStack tapped!")
-        }
+        Text("Hello, world!")
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
     }
 }
 
